92908223973890da615f2d50f9ed24e020cf2f91
apomic80 <michele.aponte@blexin.com>
2017-12-27 12:45:07 +0100

    step2 completed

--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-widget',
   templateUrl: './widget.component.html',
   styleUrls: ['./widget.component.css']
 })
-export class WidgetComponent implements OnInit {
+export class WidgetComponent {
+
+  @HostBinding('class')
+  gridClass = 'grid-stack-item';
+
+  @HostBinding('attr.data-gs-x')
+  @Input()
+  public positionX: number;
+
+  @HostBinding('attr.data-gs-y')
+  @Input()
+  public positionY: number;
+
+  @HostBinding('attr.data-gs-width')
+  @Input()
+  public width: number;
+
+  @HostBinding('attr.data-gs-height')
+  @Input()
+  public height: number;
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
